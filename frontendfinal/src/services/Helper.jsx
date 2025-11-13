1340e9be0813c9f6ac4551a0533b3206887debed
Tushar9184 <sabharwaltushar02@gmail.com>
2025-11-13 21:35:55 +0530

    Update Helper.jsx

--- frontendfinal/src/services/Helper.jsx
+++ frontendfinal/src/services/Helper.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-export const BASE_URL='http://localhost:8080';
+export const BASE_URL='https://beer-game-backend.onrender.com' ;
 export const myAxios= axios.create({
     baseURL:BASE_URL,
     
